bcd6ad948c5c2feb6682c3385c4ee2358ea34a08
averywald <averywald.dev@gmail.com>
2020-11-13 16:56:09 -0600

    initialized login component

--- hrmni/src/app/app-routing.module.ts
+++ hrmni/src/app/app-routing.module.ts
@@ -1,5 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  AngularFireAuthGuard,
+  emailVerified,
+  hasCustomClaim,
+  isNotAnonymous,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo
+} from '@angular/fire/auth-guard';
+
+// AngularFire AuthGaurd pipes
+const adminOnly = () => hasCustomClaim('admin');
+// const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+// const redirectLoggedInToItems = () => redirectLoggedInTo(['items']);
+// const belongsToAccount = (next) => hasCustomClaim(`account-${next.params.id}`);
 
 const routes: Routes = [];
 
